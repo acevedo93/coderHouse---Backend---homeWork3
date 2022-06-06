02fffd057e04d97b125a13492d7271324b1ac4d3
David <dacevedojaramillo@gmail.com>
2022-06-06 14:45:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const productos = require('./products')
+
+app.get('/productos', (req, res) => {
+    res.json({productos}) 
+})
+
+app.get('/productoRandom', (req, res) => {
+    const productsLength = productos.length
+    const randomId = Math.floor(Math.random() * productsLength)
+    res.json({producto: productos[randomId]})
+})
+
+app.listen(port, () => {
+    console.log(`server running on ${port}`)
+})
+
---
+++ products.js
@@ -0,0 +1,26 @@
+const productos = [{
+    title: 'Cuadro',
+    price: '2000',
+    id: 1
+    
+},{
+    title: 'Ipad',
+    price: '5000',
+    id:2
+}, {
+    title: 'Iphone',
+    price: '30000',
+    id:3
+}, 
+{
+    title: 'Mustang',
+    price: '400000',
+    id:4
+},
+ {
+     title: 'Chocolate',
+     price: '3000',
+     id: 5
+ }]
+
+module.exports = productos
